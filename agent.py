c3f31203fc2f251fbdaa3e76a63828a77fdef3a5
cyhuang4812 <67165862+cyhuang4812@users.noreply.github.com>
2020-06-26 09:27:04 -0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,22 +1,24 @@
 import random
-
 class Agent:
+
+	_buffer = [] #list of stories?
+	Agent neighbor #general neighbor
 
 	def __init__(self, nodeid):
 		self.id = nodeid
+		# self == this?
 		# the agent should have a buffer of recently-read stories
 		# see tryPutInBuffer()
-		# the agent should have a two small neural networks: 
+		# the agent should have a two small neural networks:
 		#  story-to-text and vice versa
 
 	def id(self):
 		return self.id
-
 	def perceiveStory(self):
 		# Asynchronously receive a story that spawns in the environment
 		pass
 
-	def sendMessage(self, message):
+	def sendMessage(self, message): #what is self?? Neighbor? the current agent?
 		pass
 
 	def receiveMessage(self, message):
@@ -29,7 +31,7 @@ class Agent:
 
 	def tryPutInBuffer(self, message):
 		## the buffer should NOT be a "buffer" type (which stores bytes)
-		## for this buffer, use a dictionary where the key is timestamp of 
+		## for this buffer, use a dictionary where the key is timestamp of
 		## when the story is processed, and value is the story itself
 		## as a buffer, it has a limit to the number of instances that can be stored
 		## when it is "full", it will not accept new stories. Should return gracefully
@@ -40,6 +42,17 @@ class Agent:
 		# make the threshold for "too old" a variable so it's easy to tweak
 
 	def step(self):
+		neighbors = names.neighbornames
+		neighbor = new Agent(self, "");
+		for key, value in neighbors.items():
+			if id(self) == key:
+				neighbor = new Agent(neighbor, value[random.randint(0, value.len() - 1)])# select random name from latter half of namelist
+				#should I match the Agent with the neighbor?  Is there already a neighbor Agent instantiated?
+		if (self._buffer().len() != 0):
+			story = _buffer[random.randint(0, _buffer.len() - 1)];
+			sendMessage(neighbor, storyToText(story)) #assuming self is story type for storyToText and self is neighbor for sendMessage
+		else:
+			return;
 		# Decide the neighbor to spread a message to
 		# Choose a story in the buffer to spread
 		# return early if there is no story in the buffer
@@ -47,7 +60,7 @@ class Agent:
 		# Send story-text to chosen neighbor (call sendMessage)
 		pass
 
-	def storyToText(self):
+	def storyToText(self): #shouldn't self be a Story value???
 		# can use dummy values before a neural network module is set up
 		pass
 

cdbff3bbdb6ff4cbbc2a6720f91736e599d2f6e3
catherinexhe <22catherineh@students.harker.org>
2020-07-20 11:52:49 -0700

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -4,13 +4,14 @@ from spacy.lang.en import English
 nlp = English()
 tokenizer = Tokenizer(nlp.vocab)
 
-def convertText(text):
-    tokenList = tokenizer(text)
-    if isinstance(tokenList, list):
-        vectors = []
-        for i in tokenList:
-            vectors.append(i.vector)
-        return vectors
-    else:
-        return tokenList.vector
-    return tokenList
+class Token:
+    def convertText(self, text):
+        tokenList = tokenizer(text)
+        if isinstance(tokenList, list):
+            vectors = []
+            for i in tokenList:
+                vectors.append(i.vector)
+                return vectors
+            else:
+                return tokenList.vector
+        return tokenList

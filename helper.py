28d2cdd31ade169c8746dbe4685225d139a0afa6
catherinexhe <22catherineh@students.harker.org>
2020-07-20 10:43:36 -0700

    update helper.py

--- helper.py
+++ helper.py
@@ -1,54 +1,69 @@
-import pymongo
 from pymongo import MongoClient
-from nltk import framenet as fn
 
 
-class Helper:
+
+class MongoHelper:
     __db = None
-    def __init__(self):
+    @classmethod
+    def __init__(cls):
         #Initialize your database connection here.
-        if Helper.__db == None:
-            Helper.__db = connect('localhost:27017/framenet')
+        client = MongoClient('127.0.0.1', 27017)
+        if MongoHelper.__db == None:
+            MongoHelper.__db = client["framenet"]
 
-    def getDatabase():
-      # Static access method. 
-      if Helper.__db == None:
-         Helper()
-      return Helper.__db
+    @classmethod       
+    def getDB(cls):
+        return MongoHelper.__db
+    @classmethod
+    def getTextFromData(cls):
+        # if (mongoClient == None)
+        #     mongoClient = MongoClient()
+        # if framenet not in dbList):
+        #     db = Helper.getDatabase()
+        texts = []
+        docs = MongoHelper.getDB().p_data.find({})
+        for i in docs:
+            txt = i["text"]
+            texts.append(txt)
+        return texts
 
-    def getTextFromData():
-        if (mongoClient == None)
-            mongoClient = MongoClient()
+    @classmethod
+    def getStoryFromData(cls):
+        # if (mongoClient == None)
+        #     mongoClient = MongoClient()
 
-        dbList = mongoClient.list_database_names()
-        if framenet not in dbList):
-            db = Helper.getDatabase()
-
-        db.p_data.find({}, {"text":1})
-
-
-    def getStoryFromData():
-        if (mongoClient == None)
-            mongoClient = MongoClient()
-
-        if framenet not in dbList):
-            db = Helper.getDatabase()
-
-        db.p_data.find({}, {"story":1})
+        # if framenet not in dbList):
+        #     db = Helper.getDatabase()
+        stories = []
+        docs = MongoHelper.getDB().p_data.find({})
+        for i in docs:
+            story = i["story"]
+            stories.append(story)
+        return stories
+    @classmethod
+    def getAllEntries(cls):
+        entries = []
+        docs = MongoHelper.getDB().p_data.find({})
+        for i in docs:
+            text = i["text"]
+            story = i["story"]
+            entry = (text,story)
+            entries.append(entry)
+        return entries
 
 #we can't find a way to access specific docs so we decided to create a method 
 #that would operate with a collection of documents that have an "index" field as well. 
-    def getEntry(index):
-        processed_data = db.p_data
-        #document = db.processed_data.find("index": index)
-        textList = getTextFromData()
-        text = textList[index]
-        #text = document["text"]
-        #text = document["story"]
-        storyList = getStoryFromData()
-        story = storyList[index]
-        entry = (text, story)
-        return entry
+    # def getEntry(index):
+    #     processed_data = db.p_data
+    #     #document = db.processed_data.find("index": index)
+    #     textList = getTextFromData()
+    #     text = textList[index]
+    #     #text = document["text"]
+    #     #text = document["story"]
+    #     storyList = getStoryFromData()
+    #     story = storyList[index]
+    #     entry = (text, story)
+    #     return entry
 
 
 
